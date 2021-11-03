71e5bdbe919338d6c75076c2ff76ed68a465797b
Mario Beltrán Alarcón <belco90@gmail.com>
2021-11-03 21:52:53 +0100

    add basic App

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+export const App = () => {
+  const [secondsOpen, setSecondsOpen] = useState(0)
+  const [clicksCount, setClicksCount] = useState(0)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSecondsOpen(secondsOpen + 1), 1000)
+    return () => clearTimeout(timer)
+  }, [secondsOpen, setSecondsOpen])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="React logo" />
+        <h1>Hello Parcel + React!</h1>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          Page has been open for <code>{secondsOpen}</code> seconds.
+        </p>
+        <p>
+          <button
+            type="button"
+            onClick={() => setClicksCount((count) => count + 1)}
+          >
+            Count is: {clicksCount}
+          </button>
+        </p>
+
+        <p>
+          <a
+            className="App-link"
+            href="https://beta.reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://parceljs.org/docs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Parcel Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+};
