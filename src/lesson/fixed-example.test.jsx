a6f078fa69cf6196f24f6009b60f909eafaea572
Mario Beltrán Alarcón <belco90@gmail.com>
2022-04-26 00:46:20 +0200

    add exercises

--- src/lesson/fixed-example.test.jsx
+++ src/lesson/fixed-example.test.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { App } from '../App'
+import userEvent from '@testing-library/user-event'
 
-it('should render a basic demo', () => {
+it('should reset counts - using fireEvent', async () => {
   render(<App />)
-  expect(screen.getByText('Hello Parcel + React!')).toBeInTheDocument()
+
+  const countButton = screen.getByRole('button')
+  expect(countButton).toHaveTextContent('Count is: 0')
+  fireEvent.click(countButton)
+  expect(countButton).toHaveTextContent('Count is: 1')
+
+  const resetInput = screen.getByLabelText(
+    /write "reset" and hit enter to reset all the counts/i,
+  )
+  fireEvent.keyDown(resetInput, {
+    code: 'Enter',
+    target: { value: 'reset' },
+  })
+  expect(countButton).toHaveTextContent('Count is: 0')
+})
+
+it('should reset counts - using userEvent', async () => {
+  render(<App />)
+
+  const countButton = screen.getByRole('button')
+  expect(countButton).toHaveTextContent('Count is: 0')
+  userEvent.click(countButton)
+  expect(countButton).toHaveTextContent('Count is: 1')
+
+  const resetInput = screen.getByLabelText(
+    /write "reset" and hit enter to reset all the counts/i,
+  )
+  await userEvent.type(resetInput, 'reset', { delay: 15 })
+  userEvent.keyboard('{enter}')
+  expect(countButton).toHaveTextContent('Count is: 0')
 })
--- src/lesson/practice.test.jsx
+++ src/lesson/practice.test.jsx
@@ -3,10 +3,55 @@
  */
 
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { App } from '../App'
+import userEvent from '@testing-library/user-event'
 
-it('should render a basic demo', () => {
+it('should reset counts - using fireEvent', async () => {
   render(<App />)
-  expect(screen.getByText('Hello Parcel + React!')).toBeInTheDocument()
+
+  const countButton = screen.getByRole('button')
+  expect(countButton).toHaveTextContent('Count is: 0')
+  fireEvent.click(countButton)
+  await fireEvent.click(countButton)
+  expect(countButton).toHaveTextContent('Count is: 2')
+
+  const resetInput = screen.getByLabelText(
+    /write "reset" and hit enter to reset all the counts/i,
+  )
+  await fireEvent.keyDown(resetInput, {
+    code: 'Enter',
+    target: { value: 'something else' },
+  })
+  expect(countButton).toHaveTextContent('Count is: 2')
+
+  await fireEvent.keyDown(resetInput, {
+    target: { value: 'reset' },
+  })
+  expect(countButton).toHaveTextContent('Count is: 2')
+
+  await fireEvent.keyDown(resetInput, {
+    code: 'Enter',
+  })
+  expect(countButton).toHaveTextContent('Count is: 0')
+})
+
+it('should reset counts - using userEvent', async () => {
+  render(<App />)
+
+  const countButton = screen.getByRole('button')
+  expect(countButton).toHaveTextContent('Count is: 0')
+  userEvent.click(countButton)
+  await userEvent.click(countButton)
+  expect(countButton).toHaveTextContent('Count is: 2')
+
+  const resetInput = screen.getByLabelText(
+    /write "reset" and hit enter to reset all the counts/i,
+  )
+  await userEvent.type(resetInput, 're')
+  await userEvent.type(resetInput, 'se', { delay: 200 })
+  await userEvent.type(resetInput, 'tt', { delay: 0 })
+  await userEvent.type(resetInput, '{backspace}', { delay: 15 })
+  await userEvent.keyboard('{enter}')
+  expect(countButton).toHaveTextContent('Count is: 0')
 })
