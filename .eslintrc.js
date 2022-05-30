34ee4abfff955a148b386b832e4cb6a76a000efe
Mario Beltrán Alarcón <belco90@gmail.com>
2022-05-31 01:08:13 +0200

    add exercises

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,10 @@ module.exports = {
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
       extends: ['plugin:testing-library/react'],
-      rules: {},
+      rules: {
+        // disable the following rules to focus on `no-wait-for-side-effects`
+        'testing-library/no-wait-for-multiple-assertions': 'off',
+      },
     },
   ],
   parserOptions: {
@@ -31,5 +34,7 @@ module.exports = {
       version: 'detect',
     },
   },
-  rules: {},
+  rules: {
+    'react/prop-types': 'off',
+  },
 }
--- src/lesson/example.test.jsx
+++ src/lesson/example.test.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { App } from '../App'
+import userEvent from '@testing-library/user-event'
+import { bulbasaur } from './fixtures'
 
-it('should render a basic demo', () => {
+const fetchMock = jest
+  .fn()
+  .mockResolvedValue({ ok: true, json: jest.fn().mockResolvedValue(bulbasaur) })
+
+beforeEach(() => {
+  global.fetch = fetchMock
+})
+
+it('should render the basic Pokémon info', async () => {
   render(<App />)
-  expect(screen.getByText('Hello Parcel + React!')).toBeInTheDocument()
+
+  const idInput = screen.getByLabelText('Pick one Pokémon by their ID')
+  userEvent.type(idInput, '1')
+
+  await waitFor(() => {
+    userEvent.click(screen.getByRole('button', { name: 'Search' }))
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+  })
 })
